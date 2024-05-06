9308d034cb3cf6b801494e94ab0d9fdf459ac577
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-05-06 00:25:45 -0400

    Day 6

---
+++ Day 6 - If-for/conditional.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	for a > 0 {
+		fmt.Println("a", a)
+		a -= 2
+	}
+
+	var b int
+	for b = 0; b < 10; b += 1 {
+		fmt.Println("b", b)
+	}
+} 
