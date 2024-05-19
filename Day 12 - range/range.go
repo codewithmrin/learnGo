ab819f967d1907004f405b4998f4ccac560569c1
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-05-19 01:23:10 -0400

    renumbered files

--- Day 12 - range/range.go
+++ Day 12 - range/range.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func main() {
 	numbers := []int{10, 20, 30, 40, 50}
-	// Order is preserved in an array
-	for _, number := range numbers {
-		fmt.Printf("Value is %d\n", number)
+	// Order is preserved
+	for i, number := range numbers {
+		fmt.Printf("Value is %d\n", i, number)
 	}
 
 	lettersMap := map[string]int{"a": 1, "b": 2, "c": 3}
-	// Order is not preserved in a map
+	// Order is not preserved
 	for key, value := range lettersMap {
 		fmt.Printf("Key: %s Value: %d\n", key, value)
 	}
